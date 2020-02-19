c1ee13ac12b7517582d4811cf0fc2b204da00546
Ivan Rodriguez-Conde <irconde@ualr.edu>
2020-02-19 10:07:56 -0600

    Minor changes in TODO comments

--- app/src/main/java/com/ualr/patterns_example/MainActivity.java
+++ app/src/main/java/com/ualr/patterns_example/MainActivity.java
@@ -15,23 +15,25 @@ public class MainActivity extends AppCompatActivity {
     private Button mSaveBtn;
     private TextView mOutputUser;
 
+    // TODO 04: We have to create an attribute of Activity class to hold a reference to the model.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO 13: We can test everything is ok with our model
-        // TODO 14: Check what information is in our database in this moment. We'll use the logcat
-        // TODO 15: Obviously we can check that everything is ok debugging
-        // TODO 16: We have to get the references to the text fields in the GUI in order to get data
+        // TODO 05: Initialize the model
+        // TODO 06: Check what information is in our database in this moment. We'll use the logcat
+        // TODO 07: Obviously we can check that everything is ok debugging
+        // TODO 08: We have to get the references to the text fields in the GUI in order to get data
         this.mIdET = findViewById(R.id.id_edit_text);
         this.mNameET = findViewById(R.id.name_edit_text);
         this.mOutputUser = findViewById(R.id.output_text_view);
-        // TODO 17: We have to get a reference of each Button in the text field in order to define the
+        // TODO 09: We have to get a reference of each Button in the text field in order to define the
         // actions triggered in response to a click action
         this.mRetrieveBtn = findViewById(R.id.retrieve_btn);
-        // TODO 18: Ctrl + D is very useful. We can duplicate a line
+        // TODO 10: Ctrl + D is very useful. We can duplicate a line
         this.mSaveBtn = findViewById(R.id.save_btn);
-        // TODO 19: We have to define the code that is invoked when the user taps on each button
-        // TODO 20: Maybe you don't remember all the methods of a class or interface...
+        // TODO 11: We have to define the code that is invoked when the user taps on each button
+        // TODO 12: Maybe you don't remember all the methods of a class or interface...
     }
 }
