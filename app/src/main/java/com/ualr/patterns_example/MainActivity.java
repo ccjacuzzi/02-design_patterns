19b74c9d2877d7ab18cc0e6ddce7fc4fb0788050
irconde <irconde@ualr.edu>
2020-09-14 15:39:34 -0500

    Minor changes

--- app/src/main/java/com/ualr/patterns_example/MainActivity.java
+++ app/src/main/java/com/ualr/patterns_example/MainActivity.java
@@ -7,13 +7,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ualr.patterns_example.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mIdET;
-    private EditText mNameET;
-    private Button mRetrieveBtn;
-    private Button mSaveBtn;
-    private TextView mOutputUser;
+    private ActivityMainBinding mBinding;
 
     // TODO 04: We have to create an attribute of Activity class to hold a reference to the model.
 
@@ -23,17 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // TODO 05: Initialize the model
         // TODO 06: Check what information is in our database in this moment. We'll use the logcat
-        // TODO 07: Obviously we can check that everything is ok debugging
-        // TODO 08: We have to get the references to the text fields in the GUI in order to get data
-        this.mIdET = findViewById(R.id.id_edit_text);
-        this.mNameET = findViewById(R.id.name_edit_text);
-        this.mOutputUser = findViewById(R.id.output_text_view);
-        // TODO 09: We have to get a reference of each Button in the text field in order to define the
-        // actions triggered in response to a click action
-        this.mRetrieveBtn = findViewById(R.id.retrieve_btn);
-        // TODO 10: Ctrl + D is very useful. We can duplicate a line
-        this.mSaveBtn = findViewById(R.id.save_btn);
-        // TODO 11: We have to define the code that is invoked when the user taps on each button
-        // TODO 12: Maybe you don't remember all the methods of a class or interface...
+        // TODO 07: We have to define the code that is invoked when the user taps on each button
     }
 }
