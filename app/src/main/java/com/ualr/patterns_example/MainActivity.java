70c27aa1441b58e420a1ce2186812ca4e1e2dc9e
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-02-27 11:48:11 -0600

    Initial commit

--- app/src/main/java/com/ualr/patterns_example/MainActivity.java
+++ app/src/main/java/com/ualr/patterns_example/MainActivity.java
@@ -10,15 +10,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.ualr.patterns_example.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO 02: Add a new view binding member to the class
 
     private ActivityMainBinding mBinding;
+
+
 
     // TODO 04: We have to create an attribute of Activity class to hold a reference to the model.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // TODO 03: Set content view using the new binding member.
+        // These two lines link the xml layout to the java code
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        
         // TODO 05: Initialize the model
         // TODO 06: Check what information is in our database in this moment. We'll use the logcat
         // TODO 07: We have to define the code that is invoked when the user taps on each button
